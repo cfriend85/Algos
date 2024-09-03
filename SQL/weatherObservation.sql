abb421a9977082c507c6c5a1a663369f63eb0c7b
Charles Friend <c.friend85@yahoo.com>
2024-09-03 16:45:07 -0600

    sql challenges

--- SQL/weatherObservation.sql
+++ SQL/weatherObservation.sql
@@ -44,3 +44,7 @@ Select CAST(MAX(LAT_N) as decimal(10,4)) from Station where LAT_N < 137.2345; --
 --Query the Western Longitude (LONG_W) for the largest Northen Latitude (LAT_N) in Station that is less than 137.2345. Round your answer to 4 decimal places.
 
 Select CAST(LONG_W as decimal(10,4)) from Station where LAT_N = (Select MAX(LAT_N) from Station where LAT_N < 137.2345);
+
+--Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 deimal places.
+
+Select CAST(MIN(LAT_N) as decimal(10,4)) from STATION where LAT_N > 38.7780;
